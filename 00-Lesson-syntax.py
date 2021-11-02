8d6604d3261e1554813534bc2803a785a8e4e0a4
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-11-02 12:27:50 +0600

    syntax code

---
+++ 00-Lesson-syntax.py
@@ -0,0 +1,34 @@
+# Execute python Syntax
+print("Hello world")
+
+# Python identation
+x = 10
+if( x > 0):
+    print('x greater than number') #This indentation because refers to the spaces at the beggining of a code 
+
+# skip indentation 
+n = 20
+if n < 10:
+    print('smaller error') # python will give an error if you skip identation 
+
+# The number of spaces is up to you as a programmer, but it has to be at least one.
+if 5 > 2:
+    print('Five greater than two')
+if 5 > 2 :
+    print('Five greater than two')
+
+# You have to use same number of spaces in the same block of code, otherwise python  will give you an error
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+
+# Python Variables
+x = 10
+y = "Comments"
+
+print(x)
+print(y)
+
+# This is comment
+print('this is a Comment')
+
