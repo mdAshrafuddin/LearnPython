9ffe608182f99c58e6bbf8ff60509377bf90e588
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-11-02 21:06:28 +0600

    02 project calculation

---
+++ 02-Project-calculation.py
@@ -0,0 +1,38 @@
+# Make a convertion from kg to lbs
+# Use this formula: $lbs = kg * 2.2046$
+# Make the cell to use variables
+
+kg = int(input("Enter number: "))
+
+lbs = kg * 2.2046
+
+print(lbs)
+
+"""
+Make calculation of circumference of a circle
+The cell should define the radius
+Calculate the circumference by the formula: c = 2* pi * r
+Define pi = 3.14 (use variable name pi)
+Let the cell return the circumference`
+"""
+r = int(input("Enter Number: "))
+pi = 3.14
+
+c = 2 * pi * r
+
+print(c)
+
+"""
+Assign a variable to a any number (integer).
+Multiply the number by 3.
+Add 6 to the result from bullet 2.
+Divide this number by 3.
+Subtract the number from bullet 1 from the answer in bullet 4.
+The answer is 2.
+"""
+number = 45
+number = number * 3
+number = number + 6
+number = number / 3
+number = number - 45
+print(number)
