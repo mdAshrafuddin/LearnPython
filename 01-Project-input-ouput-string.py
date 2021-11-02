0b088a490df3813e074c20443aea1a6d4144c160
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-11-02 13:43:22 +0600

    Project input output

---
+++ 01-Project-input-ouput-string.py
@@ -0,0 +1,17 @@
+# input value from user
+
+# Make your first string value from user
+x = str(input('Please eneter Any value: ')) 
+print(x)
+
+# User input to get Hello and Name from user
+hello = str(input("Please Typing here Hello : "))
+name  = str(input("Please Typing your name : "))
+
+print(hello + "! " + name) 
+
+# User input to get the name from user
+# print Like Capital Letter and name capitalized  by using .upper()
+nameCap = str(input("Please enter value: "))
+
+print(nameCap.upper())
